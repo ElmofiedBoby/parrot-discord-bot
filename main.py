0cec8c72567b1f7fc7a9d8602b50a4ae89846333
Nithin Joseph <nithjoe1@gmail.com>
2021-09-22 01:28:26 -0400

    added updates

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+from discord.voice_client import VoiceClient
+from discord import FFmpegPCMAudio
+import requests
 import os
+import music
 
 client = discord.Client()
+bot = commands.Bot(command_prefix="!")
 
 @client.event
 async def on_ready():
@@ -20,16 +27,21 @@ async def on_message(message):
     if message.content.startswith('b') or message.content.startswith('B'):
         emoji_b = '\U0001F171' # 🅱
         await message.add_reaction(emoji_b)
+    
+    if message.content.startswith('!download'):
+        os.system('rm song.*')
+        music.play(message.content[6:])
+
+    #if message.content.startswith('!play'):
+
+
 
 @client.event
 async def on_reaction_add(reaction, user):
     quotes = client.get_channel(888832318511386644)
     if user.bot:
         return
-    # I do not actually recommend doing this.
     if reaction.emoji == '\U0001F4F8':
         await quotes.send("\"{0}\" -{1}".format(reaction.message.content, reaction.message.author))
-
-
 
 client.run(os.getenv('TOKEN'))
