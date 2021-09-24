8931d7e0ea4cd2a7f77e53761faba60695634397
Nithin Joseph <nithjoe1@gmail.com>
2021-09-24 18:23:57 -0400

    added on_ready()

--- bot.py
+++ bot.py
@@ -12,14 +12,19 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
                    description='nithin\'s personal music/fun bot')
 
-@commands.Cog.listener()
-async def on_reaction_add(chat, reaction, user):
+@bot.event
+async def on_reaction_add(reaction, user):
     id = 888832318511386644
-    quotes = bot.get_channel(id)
+    quotes = bot.get_channel(888832318511386644)
     if user.bot:
         return
     if reaction.emoji == '\U0001F4F8':
         await quotes.send("\"{0}\" -{1}".format(reaction.message.content, reaction.message.author))
+
+@bot.event
+async def on_ready():
+    print('Logged in as {0} ({0.id})'.format(bot.user))
+    print('------')
 
 bot.add_cog(Music.Music(bot))
 bot.add_cog(Chat.Chat(bot))
