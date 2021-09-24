b9d57a12648f695c98a999096507ba879696e3f0
Nithin Joseph <nithjoe1@gmail.com>
2021-09-24 18:09:58 -0400

    redundant code removed

--- Music.py
+++ Music.py
@@ -8,21 +8,7 @@ class Music(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-    
-    @commands.Cog.listener()
-    async def on_message(self, message):
-
-        if message.author == self.bot.user:
-            return
-
-        if "cringe" in message.content:
-            emoji_disgust = '\U0001F922'
-            await message.add_reaction(emoji_disgust) # 🤢
-
-        if message.content.startswith('b') or message.content.startswith('B'):
-            emoji_b = '\U0001F171' # 🅱
-            await message.add_reaction(emoji_b)
-
+        
     @commands.command()
     async def join(self, ctx, *, channel: discord.VoiceChannel):
         """Joins a voice channel"""
